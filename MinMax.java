c3eadfa719ce143b1b5e16018722e694fa030f05
nsuleymanov23805 <nihad.suleymanov007@gmail.com>
2026-02-20 10:50:12 +0400

    2 complete

--- MinMax.java
+++ MinMax.java
@@ -22,6 +22,24 @@ public class MinMax {
         return max;
     }
 
+    public static int[] findminmax(int[] arr) {
+
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return new int[]{min, max};
+}
+
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
